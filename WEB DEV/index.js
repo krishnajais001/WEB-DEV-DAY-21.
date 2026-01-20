37b103484c0727642b103cf967eaf37e20605ef0
Krishna Kumar <krishnajais7091@gmail.com>
2026-01-21 02:01:28 +0530

    commit

---
+++ WEB DEV/index.js
@@ -0,0 +1,102 @@
+/* Var */
+/* 
+function example() {
+    var x = 10;
+    console.log(x);
+}
+
+example();       // 10
+console.log(x);       // Throws ReferenceError: x is not defined
+*/
+// Reassignment and Redefinition using var
+
+
+/* Operators in JavaScript */
+
+/* 🌟 Arithmetic Operators */ 
+let number1 = 5;
+let number2 = 9;
+let Sum = number2 + number1; 
+let Sub = number2 - number1;
+let Mul = number2 * number1;
+let Div = number2 / number1;
+let Mod = number2 % number1;
+let Pow = number2 ** number1;
+console.log("Sum:"+Sum);
+console.log("Sub:"+Sub);
+console.log("Mul:"+Mul);
+console.log("Div:"+Div);
+console.log("Mod:"+Mod);
+console.log("Pow:"+Pow);
+/*📍Assignment Operators -> = */
+
+
+/* 
+📍Assignment Operators
+
+🌟Comparison Operators
+
+📍Logical Operators
+
+🌟Bitwise Operators
+
+📍Ternary Operators 
+*/
+
+// let x = 10;
+// let y = 4;
+
+// x++;
+// console.log(x);      // 11
+
+// y--; 
+// console.log(y);     //2
+
+
+//Pre increment/decrement
+// let x = 10;
+// let y = 4;
+
+// console.log(++x);    // 11
+// console.log(--y);    // 3
+// console.log(x);      // 11
+// console.log(y);      // 3
+
+//Post increment/decrement
+// let a = 10;
+// let b = 4;
+// console.log(a++);      // 10
+// console.log(b--);      // 4
+
+// console.log(a);        // 11
+// console.log(b);        // 3
+
+// let string1 = "Mai pal do pal ka shayar hoon";
+
+// let string2 = "Pal do pal meri kahani hai";
+
+// let multiLineString = `
+//     ${string1},
+//     ${string2}`
+
+// console.log(multiLineString);
+
+// let weight = 65;
+
+// if (weight > 70) {
+//     console.log("You are Overweight");
+// } else if (weight > 50 && weight <= 70) {
+//     console.log("You are Fit");
+// } else {
+//     console.log("You are Underweight");
+// }
+
+
+let string1 =  "Namaste";
+console.log(typeof(string1));      // string
+
+let string2 = 'Namaste';
+console.log(typeof(string2));      // string
+
+let string3 = `Namaste`;
+console.log(typeof(string3));      // string
