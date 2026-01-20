812b4f2b57963bfa37faf27a235b4014fcfc78b7
Krishna Kumar <krishnajais7091@gmail.com>
2026-01-21 02:41:01 +0530

    update

---
+++ 1.js
@@ -0,0 +1,33 @@
+let dayCount =1;
+let actualDay = '';
+switch(dayCount){
+case 1:
+    actualDay='Monday';
+    break;
+    
+case 2:
+    actualDay='Tuesday';
+    break;
+
+case 3:
+    actualDay='Wednesday';
+    break;
+
+case 4:
+    actualDay='Thrusday';
+    break;
+
+case 5:
+    actualDay='Friday';
+    break;
+
+case 6:
+    actualDay='Saturday';
+    break;
+
+case 7:
+    actualDay='Sunday';
+    break;
+}
+
+console.log(actualDay);
